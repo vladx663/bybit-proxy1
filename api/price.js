7ed5bfc9158025da02cf71d8a201595e61d0ba88
vladx663 <2123176@gmail.com>
2025-06-12 00:12:51 +0300

    Update price.js

--- api/price.js
+++ api/price.js
@@ -1,1 +1,13 @@
 
+const fetch = require('node-fetch');
+
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://api.bybit.com/v2/public/tickers?symbol=BTCUSDT');
+    const data = await response.json();
+    const price = data.result[0].last_price;
+    res.status(200).json({ price });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch price" });
+  }
+}
